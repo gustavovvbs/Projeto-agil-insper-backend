f9c02b6f0b956787aae8724db548234c4a95489b
haseduu <haseduuu@gmail.com>
2024-11-01 11:02:32 -0300

    create the routes for it

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, Blueprint
 from models import coordenador, estudante, professor, user 
-from controllers.auth_controller import register_user, login_user
+from controllers.auth_controller import register_user, login_user, create_token_and_send_email
 from utils.auth_decorator import role_required
 
 
@@ -20,3 +20,15 @@ def login():
     response = login_user(data)
 
     return response 
+
+@auth_routes.route("/esqueci-senha/<str:idd>", methods=["GET"])
+def retrieve_password(idd):
+    response = create_token_and_send_email(idd)
+    return response
+
+
+@auth_routes.route("/recuperar/<str:token>", methods=["POST"])
+def retrieve_password(token):
+    data = request.get_json()
+    response = create_token_and_send_email(token, data)
+    return response
