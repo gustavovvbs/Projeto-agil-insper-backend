99fbff48e08b296bbd102df85b77577f779ba6e2
haseduu <haseduuu@gmail.com>
2024-11-02 16:16:25 -0300

    professor routes implementation

--- controllers/professor_controller.py
+++ controllers/professor_controller.py
@@ -1,5 +1,6 @@
 from models.professor import Professor 
-
+from models.projeto import Projeto
+from aplicacao_controller import get_aplicacao_by_projeto
 def get_by_id(id: str):
     professor = Professor.get_by_id(id)
     if not professor:
@@ -12,4 +13,28 @@ def get_all():
     if not professores:
         return {"message": "No professors found"}, 404
     professores = [professor.dict() for professor in professores]
-    return professores
+    return professores, 200
+
+def get_projects_by_professor(id):
+    projects = Projeto.get_all_by_professors(id)
+    if not projects:
+        return {"message": "No projects found"}, 404
+    projects = [project.dict() for project in projects]
+    return projects, 200
+
+def get_project_by_id(id):
+    project = Projeto.get_by_id(id)
+    if not project:
+        return {"message": "No project found"}, 404
+    return project, 200
+
+def get_applications_by_project_id(id):
+    project = Projeto.get_by_id(id)
+    if not project:
+        return {"message": "No project found"}, 404
+    applications = get_aplicacao_by_projeto(id)
+    if not applications:
+        return {"message": "No applications found for this project"}, 200
+    applications = [application.dict() for application in applications]
+    return applications, 200
+
--- routes/professor_routes.py
+++ routes/professor_routes.py
@@ -3,7 +3,8 @@ from utils.jwt_auth import decode_jwt_token
 from models.projeto import Projeto
 from models.processo_seletivo import ProcessoSeletivo
 from models.professor import Professor
-from controllers.professor_controller import get_by_id, get_all
+from controllers.professor_controller import get_by_id, get_all, get_projects_by_professor, get_project_by_id, get_applications_by_project_id
+from controllers.aplicacao_controller import get_aplicacao
 from bson import ObjectId
 from flask import Blueprint, request, jsonify
 
@@ -21,4 +22,30 @@ def get_all_professores():
 
     return response
 
-# @professor_routes.route()
+@professor_routes.route("/projetos/professor/<id>", methods=["GET"])
+@role_required(["professor"])
+def get_all_projects(id):
+    response = get_projects_by_professor(id)
+
+    return response
+
+@professor_routes.route("/projetos/<id>", methods=["GET"])
+@role_required(["professor"])
+def get_project(id):
+    response = get_project_by_id(id)
+
+    return response
+
+@professor_routes.route("/projetos/<id>/aplicacoes", methods=["GET"])
+@role_required(["professor"])
+def get_project_applications(id):
+    response = get_applications_by_project_id(id)
+    
+    return response
+
+
+@professor_routes.route("/projetos/<id>/aplicacoes/<id_aplicacao>", methods=["GET"])
+@role_required(["professor"])
+def get_application(id, id_aplicacao):
+    response = get_aplicacao(id_aplicacao)
+    return response
