547e8de3f2487371588bd07911b56574de243184
YouCanCallMeGus <gustavosantanas.2019@gmail.com>
2024-11-02 18:19:27 -0300

    auth_register started and student tests done

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -26,6 +26,21 @@ def test_auth_login_professor_400():
 
     assert answer.status_code in (400,401)
 
+def test_auth_register_professor_201():
+    headers = {}
+    url = 'https://projeto-agil-insper-backend.onrender.com/auth/register'
+    data_professor = {
+        "nome": "Professor2",
+        "email": "professor2@insper.com.br",
+        "curso": '',
+        "Semestre": '',
+        "role": "professor",
+        "password": '',
+        "area_pesquisa": 'teste',
+        "descricao": 'teste'
+    }
+    answer = requests.post(url, headers=headers, json=data_professor)
+    assert answer.status_code == 201
 
 #auth student
 def test_auth_login_student_200():
@@ -51,6 +66,22 @@ def test_auth_login_student_400():
     answer = requests.post(url,headers=headers,json=data)
 
     assert answer.status_code in (400,401)
+
+def test_auth_register_student_201():
+    headers = {}
+    url = 'https://projeto-agil-insper-backend.onrender.com/auth/register'
+    data_estudante = {
+    "nome": "Gustavo",
+    "email": "gustavoss8@al.insper.edu.br",
+    "curso": "Ciência da Computação",
+    "Semestre": "segundo",
+    'role': "estudante",
+    "password": None,
+    "area_pesquisa": None,
+    "descricao": None
+    }
+    answer = requests.post(url, headers=headers, json=data_estudante)
+    assert answer.status_code == 201
     
 
 #auth coordination
@@ -78,5 +109,20 @@ def test_auth_login_coordinator_400():
 
     assert answer.status_code in (400,401)
     
-
+def test_auth_register_coordinator_201():
+    headers = {}
+    url = 'https://projeto-agil-insper-backend.onrender.com/auth/register'
+    
+    data_coordenador = {
+        "nome": "Coordenador2",
+        "email": "coordenador2@insper.com.br",
+        "curso": None,
+        "Semestre": None,
+        'role': "coordenador",
+        "password": None,
+        "area_pesquisa": None,
+        "descricao": None
+    }
+    answer = requests.post(url, headers=headers, json=data_coordenador)
+    assert answer.status_code == 201
 
