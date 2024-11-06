3be53550302c5a49c7051f6663babd79d1da3adb
Eduardo Hasegawa <159029556+haseduu@users.noreply.github.com>
2024-11-06 13:20:16 -0300

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = 'sciconnectinsper@gmail.com'
-    MAIL_PASSWORD = os.eviron.get('EMAIL_PASSWORD')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = 'sciconnectinsper@gmail.com'
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
