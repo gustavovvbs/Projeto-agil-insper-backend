1913f8f610d37867df0cf13eebf74122ff8be9db
Eduardo Hasegawa <159029556+haseduu@users.noreply.github.com>
2024-11-06 13:14:08 -0300

    Update config.py

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'your-email@gmail.com'
-    MAIL_PASSWORD = 'your-app-password'
-    MAIL_DEFAULT_SENDER = 'your-email@gmail.com'
+    MAIL_USERNAME = 'sciconnectinsper@gmail.com'
+    MAIL_PASSWORD = os.eviton.get('EMAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = 'sciconnectinsper@gmail.com'
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
